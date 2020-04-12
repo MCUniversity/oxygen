e2a8e35338de809d60e0f9de8785a00c0165f26a
MCUniversity <mcuniversity@users.noreply.github.com>
2020-04-12 18:30:15 +0200

    Config, event listeners, command listeners, your first command. | How to code a discord bot | Part 2
    Part 2 of my new series on how to code a discord bot. We went thru creating a bot config, what are event listeners, what are message listeners, made a command listener and setup the first command(!ping).

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 
+const config = require("./config.json")
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on('message', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.indexOf(config.PREFIX) !== 0) return;
+    const args = message.content.slice(config.PREFIX.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase();
+
+    if(command=="ping"){
+        let msg = await message.channel.send("Calculating...")
+        msg.edit(`My ping is ${msg.createdTimestamp - message.createdTimestamp} ms.`)
+    }
+
+
+
+
 })
 
 
@@ -11,6 +29,4 @@ client.on('ready', () => {
 
 
 
-
-
-client.login("")
+client.login(config.TOKEN)
