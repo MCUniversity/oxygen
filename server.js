054ddbf54a82ded5ec1526f4c8e1d57341382bd6
MCUniversity <mcuniversity@users.noreply.github.com>
2020-04-19 17:28:25 +0200

    How to code a discord bot part 3
    Part 3 of my series on how to code a discord bot. We went thru setting the bot's status, what an embed is (we made a help cmd), and join/leave messages.

--- server.js
+++ server.js
@@ -5,6 +5,44 @@ const config = require("./config.json")
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
+
+    client.user.setActivity(`Do !help to get help.`, {type:"PLAYING"})
+    let c = 1;
+    setInterval(function(){
+        if(c==0){
+            client.user.setActivity(`Do !help to get help.`, {type:"PLAYING"})
+            //console.log(`Do !help to get help.`)
+            c++;
+        } else if(c==1){
+            client.user.setActivity(`Do !info to get info about me`, {type:"PLAYING"})
+            //console.log(`Do !info to get info about me`)
+            c++;
+        } else if(c==2){
+            client.user.setActivity(`I am an awsome bot`, {type:"PLAYING"})
+            //console.log(`I am an awsome bot`)
+            c=0;
+        }
+
+
+    }, 60000) //60 seconds
+
+
+})
+
+client.on('guildMemberAdd', member => {
+    let embed = new Discord.MessageEmbed()
+    .setColor("fffffc")
+    .setTimestamp()
+    .addField("New member joined", `Welcome, ${member.user.tag}.`)
+    member.guild.channels.cache.get(config.WELCOMECHANNEL).send(embed)
+})
+
+client.on('guildMemberRemove', member => {
+    let embed = new Discord.MessageEmbed()
+    .setColor("fffffc")
+    .setTimestamp()
+    .addField("Member left", `Goodbye, ${member.user.tag}.`)
+    member.guild.channels.cache.get(config.WELCOMECHANNEL).send(embed)
 })
 
 client.on('message', async (message) => {
@@ -16,6 +54,13 @@ client.on('message', async (message) => {
     if(command=="ping"){
         let msg = await message.channel.send("Calculating...")
         msg.edit(`My ping is ${msg.createdTimestamp - message.createdTimestamp} ms.`)
+    } else if (command=="help") {
+        let embed = new Discord.MessageEmbed()
+        .setColor("fffffc") //ffffff
+        .setTimestamp()
+        .setFooter("Made by MCUniversity")
+        .addField("Help menu", "My commands are: \n!ping - shows the bot's ping\n!help - shows this embed")
+        message.channel.send(embed)
     }
 
 
